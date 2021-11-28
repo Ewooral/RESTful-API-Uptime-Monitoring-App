24e2290bf71ea0edc6505d69c7c000de9bbe9d3a
Ewooral <boahen.cos@gmail.com>
2021-11-28 20:35:33 +0000

    functionality to return JSON created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /**
+ * Boahen Owusu-Ewooral Boahen
  * Primary file for the RESTful API
  */
 
@@ -33,25 +34,51 @@ req.on('data', function(data){
 });
 req.on('end', function(){
       buffer += decoder.end();
-
-      // Choose the handler this request should go to
-      // send the response 
- res.end('Hello, World\n');
-
-// Log the request path
-console.log(`Request received on path ${trimmedPath} with the method: ${method}
-and with these query string parameters: ${JSON.stringify(queryStringObject)} `);
-
-console.log("Request received with these headers: " + JSON.stringify(headers));
-console.log("Request received with this payload: " + buffer);
- 
-
+      // Choose the handler this request should go to. If one is not found, use the notFound handler
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+      // construct the data object to send to the handler
+      var data = {   'trimmedPath' : trimmedPath,
+                     'queryStringObject' : queryStringObject,
+                     'method' : method,  
+                     'headers' : headers,
+                     'payload' : buffer,     
+                  };
+      // Route the request to the handler specified in the router
+      chosenHandler(data, function(statusCode, payload){
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+      // Convert the payload to a string
+      var payloadString = JSON.stringify(payload);
+      // Return the response
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log('Returning this response: ', statusCode, payloadString);
+      });
+      // send the response    
+//  res.end('Hello, World\n');
    });
-
 });
-
-
  // start the server, and have it listen on port 5000
  server.listen(5000, function(){
     console.log('Server is running on port 5000');
  });
+// Define the handlers
+var handlers = {};
+
+// Sample handler
+handlers.sample = function(data, callback){
+// Callback an http status code, and a payload object
+callback(406, {'name': 'sample handler'});
+};
+
+// Not found handler
+handlers.notFound = function(data, callback){
+callback(404);
+};
+ //Define a request router
+ var router = {
+    'sample' : handlers.sample
+ }

720e37cc29b89c7211ed5e60fc28c2e5a3abf006
Ewooral <boahen.cos@gmail.com>
2021-11-28 02:25:27 +0000

    created a path that logs requests

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Primary file for the RESTful API
+ */
+
+// Dependencies
+ const http = require('http');
+ const url = require('url');
+ // The server should respond to al requests with a string
+ let server = http.createServer(function(req, res){
+
+//  Get the URL and parse it 
+const parseUrl = url.parse(req.url, true);
+// Get the path
+const path = parseUrl.pathname;
+let trimmedPath = path.replace(/^\/+|\/+$/g, ''); // this trims off any extranious slashes
+// send the response 
+ res.end('Hello, World\n');
+// Log the request path
+console.log(`Request received on path ${trimmedPath}`);
+   
+ });
+
+
+ // start the server, and have it listen on port 5000
+ server.listen(5000, function(){
+    console.log('Server is running on port 5000');
+ });

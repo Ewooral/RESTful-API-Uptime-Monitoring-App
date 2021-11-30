155a83a1dcf8e5dc3f7333aa838335a9ee6b615c
Ewooral <boahen.cos@gmail.com>
2021-11-30 00:58:29 +0000

    configuration added to the app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
  const http = require('http');
  const url = require('url');
  var StringDecoder = require('string_decoder').StringDecoder;
+ var config = require('./config');
  // The server should respond to al requests with a string
  var server = http.createServer(function(req, res){
 
@@ -62,8 +63,8 @@ req.on('end', function(){
    });
 });
  // start the server, and have it listen on port 5000
- server.listen(5000, function(){
-    console.log('Server is running on port 5000');
+ server.listen(config.port, function(){
+    console.log(`Server is running on port, ${config.httpPort} in ${config.envName} mode`);
  });
 // Define the handlers
 var handlers = {};
